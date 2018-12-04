1173151d6143512028e205fcf32b31db8df251f8
seeduardo <romenflatiron@gmail.com>
2018-12-04 11:31:00 +0000

    added some bands to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Band.create(name: "Best Band Ever", location: "London", bio: "The clue's in the name")
+Band.create(name: "Another Band", location: "Bognor", bio: "We make music innit")
+Band.create(name: "Oh Look More Musicians", location: "Hell", bio: "Turn it up, I'm deaf")
+Band.create(name: "Total Cack", location: "London", bio: "Oh my god that is a horrible racket")
+Band.create(name: "Meh to Names", location: "Vienna", bio: "We're sad we don't have many fans")
