18163460a82eac3297f8239446f72e0e098b49ca
kbberker <kbbuk90@gmail.com>
2018-12-04 11:26:44 +0000

    made changes to model files and updated Gemfile to incl bcrypt

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,7 +1,5 @@
 class Instrument < ApplicationRecord
 
-  has_many :openings
-  has_many :bands, through: :openings
   has_many :musician_instruments
   has_many :musicians, through: :musician_instruments
 
